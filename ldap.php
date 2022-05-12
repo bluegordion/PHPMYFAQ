ed8bf037c5674dbea55c32de0118d65cbee38cf5
bluegordion <102012911+bluegordion@users.noreply.github.com>
2022-05-12 08:50:38 +0300

    Create ldap.php

---
+++ ldap.php
@@ -0,0 +1,6 @@
+<?php
+$PMF_LDAP['ldap_server'] = 'ldap://192.168.60.10';
+$PMF_LDAP['ldap_port'] = '389';
+$PMF_LDAP['ldap_user'] = 'blabla\helpdesk';
+$PMF_LDAP['ldap_password'] = '135306Ask';
+$PMF_LDAP['ldap_base'] = 'CN=help desk,OU=05-ServiceAccount,OU=03-Users,DC=blabla,DC=com';
